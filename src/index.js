787f62316b880aa0d498884e6c4364bb23193160
Lilian Montero <lilianmonterolopez@gmail.com>
2020-11-08 20:13:44 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery';
+import 'popper.js';
+import 'bootstrap';
+import './App.css';
+import App from './App';
+
+
+ReactDOM.render(
+  
+    <App />,
+  
+  document.getElementById('root')
+);
+
+
